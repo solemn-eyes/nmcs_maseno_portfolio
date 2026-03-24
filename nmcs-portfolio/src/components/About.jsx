109c8e308f9f6cb97871fbda8d304e67fcc93352
Jerome Maunga <jerhomemaunga@gmail.com>
2026-03-24 15:31:10 +0300

    Simply the beginning

---
+++ nmcs-portfolio/src/components/About.jsx
@@ -0,0 +1,15 @@
+function About() {
+  return (
+    <section id="about">
+      <h2>Maseno University Chapter</h2>
+      <p>
+        The NMCS Maseno Chapter is a vibrant community of students dedicated
+        to faith, unity, and service. We engage in spiritual nourishment,
+        leadership training, and outreach programs within and beyond campus.
+      </p>
+    </section>
+  )
+}
+
+export default About
+
